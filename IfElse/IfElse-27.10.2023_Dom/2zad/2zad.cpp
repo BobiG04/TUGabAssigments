2c611c19c962c9a8370e06d0baf1d5c112650b06
Bogomil Ivanov <bogomil_3098@abv.bg>
2023-12-06 19:07:23 +0200

    Code-06.12.2023

---
+++ IfElse/IfElse-27.10.2023_Dom/2zad/2zad.cpp
@@ -0,0 +1,22 @@
+﻿// Напишете програма която да извършва следните операции :
+// - да се въведе едно цяло число
+// - да се опреди дали е четно или нечетно
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+    int a;
+
+    scanf_s("%d", &a);
+
+    if (a %= 2) {
+        printf("Chisloto e nechetno");
+    }
+    else {
+        printf("Chisloto e chetno");
+    }
+
+    return 0;
+}
---
+++ IfElse/IfElse-27.10.2023_Dom/6zad/6zad.cpp
@@ -0,0 +1,26 @@
+﻿// Да се напише програма, която въвежда стойности за три числа(х, у и z) и извежда на екрана стойността 
+// на най - голямото от тях.
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+    int x, y, z;
+
+    scanf_s("%d", &x);
+    scanf_s("%d", &y);
+    scanf_s("%d", &z);
+
+    if (x > y && x > z) {
+        printf("%d", x);
+    }
+    else if (y > x && y > z) {
+        printf("%d", y);
+    }
+    else {
+        printf("%d", z);
+    }
+
+    return 0;
+}
