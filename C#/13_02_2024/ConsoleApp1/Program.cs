3370b97f093852b122a448bf271c916fc7745e70
Bogomil Ivanov <bogomil_3098@abv.bg>
2024-02-13 10:54:37 +0200

    13.02.2024_Code

---
+++ C#/13_02_2024/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+            Console.WriteLine(" The size of int: " + sizeof(int));
+            Console.WriteLine(" The size of uint: " + sizeof(uint));
+            Console.WriteLine(" The size of int16: " + sizeof(Int16));
+            Console.WriteLine(" The size of int32: " + sizeof(Int32));
+            Console.WriteLine(" The size of int64: " + sizeof(Int64));
+			Console.WriteLine(" The size of double: " + sizeof(double));
+			Console.WriteLine(" The size of float: " + sizeof(float));
+			Console.WriteLine(" The size of byte: " + sizeof(byte));
+			Console.WriteLine(" The size of sbyte: " + sizeof(sbyte));
+			Console.WriteLine(" The size of short: " + sizeof(short));
+			Console.WriteLine(" The size of ushort: " + sizeof(ushort));
+			Console.WriteLine(" The size of long: " + sizeof(long));
+			Console.WriteLine(" The size of ulong: " + sizeof(ulong));
+			Console.WriteLine(" The size of char: " + sizeof(char));
+			Console.WriteLine(" The size of decimal: " + sizeof(decimal));
+			Console.WriteLine(" The size of bool: " + sizeof(bool));
+			
+        }
+	}
+}
