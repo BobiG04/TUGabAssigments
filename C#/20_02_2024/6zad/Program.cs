f3cd0e9837f3d732541d6c8466e112d41b2854d6
Bogomil Ivanov <bogomil_3098@abv.bg>
2024-02-20 15:08:48 +0200

    Code-20.02.2024

--- C#/20_02_2024/6zad/Program.cs
+++ C#/20_02_2024/6zad/Program.cs
@@ -7,14 +7,18 @@ namespace EnterPosNumb
 		static void Main(string[] args)
 		{
 
-			int numb;
+			int numb, sum=0;
 
 			numb = int.Parse(Console.ReadLine());
+			sum += numb;
 
 			for (int i = 0; numb != 0; i++)
 			{
 				numb = int.Parse(Console.ReadLine());
+				sum += numb;
 			}
+
+			Console.WriteLine(" The final sum is: "+sum);
 
 		}
 	}
