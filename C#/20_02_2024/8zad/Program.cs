727467a2c18dfe970c8405a249c4f6758afbaf2a
Bogomil Ivanov <bogomil_3098@abv.bg>
2024-02-20 14:19:56 +0200

    Code-20.02.2024

---
+++ C#/20_02_2024/8zad/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace PineTree
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			int k;
+
+            Console.WriteLine("Star or Number? \n 1 = Star \n 2 = Number");
+			k = int.Parse(Console.ReadLine());
+
+			PineTree(k);
+
+		}
+
+		static void PineTree(int k)
+		{
+
+			int n;
+			Console.WriteLine("Pine tree width: ");
+			n = int.Parse(Console.ReadLine());
+
+			switch (k)
+			{
+				case 1:
+					for (int i = 0; i <= n; i++)
+					{
+						for (int j = 1; j <= n - i; j++)
+						{
+							Console.Write(" ");
+						}
+
+						for (int l = 1; l <= i * 2 - 1; l++)
+						{
+							Console.Write("*");
+						}
+
+						Console.WriteLine();
+					}
+					break;
+				case 2:
+					for (int i = 0; i < n; i++)
+					{
+						for (int j = 0; j < n - i; j++)
+						{
+							Console.Write(" ");
+						}
+
+						for (int l = i; l >= 0; l--)
+						{
+							Console.Write(l);
+						}
+
+						for (int m = 1; m <= i; m++)
+						{
+							Console.Write(m);
+						}
+
+						Console.WriteLine();
+					}
+					break;
+				default:
+                    Console.WriteLine("Error");
+					break;
+            }
+
+		}
+	}
+}
