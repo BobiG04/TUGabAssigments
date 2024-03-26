34a630797a6fcaf43f5d0a3bc568bc8b973d5c56
Bogomil Ivanov <bogomil_3098@abv.bg>
2024-03-26 15:07:19 +0200

    Code-26.03.2024

---
+++ C#/Anniversary_CST/AnniversaryCard/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		DrawCard();
+	}
+
+	static void DrawCard()
+	{
+		string message1 = "Happy 40th Anniversary";
+		string message2 = "CST";
+
+		Console.WriteLine("┌──────────────────────────────────────────────────┐");
+
+		for (int i = 0; i < 18; i++)
+		{
+			if (i == 8)
+			{
+				Console.WriteLine("│              " + message1 + "              │");
+			} else if (i == 9)
+			{
+				Console.WriteLine("│                       " + message2 + "                        │");
+			}
+			else
+			{
+				Console.WriteLine("│                                                  │");
+			}
+		}
+
+		Console.WriteLine("└──────────────────────────────────────────────────┘");
+	}
+}
