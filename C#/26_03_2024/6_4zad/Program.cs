4d33e77ac5a131a28b79412713864528a5cbd12a
Bogomil Ivanov <bogomil_3098@abv.bg>
2024-03-26 14:55:44 +0200

    Code-26.03.2024

---
+++ C#/26_03_2024/6_4zad/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _6_4zad
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			double[] arr = new double[n];
+
+			double sum;
+			double avr;
+
+			Calcuation.Calc(out sum, out avr, arr);
+
+            Console.WriteLine("The sum of all elements is: " + sum);
+            Console.WriteLine("The avarage of all elements is: " + avr);
+        }
+	}
+
+	public class Calcuation
+	{
+		public static void Calc(out double sum, out double avr, params double[] array) 
+		{ 
+			
+			sum = 0.0;
+			avr = 0.0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+			avr = sum / array.Length;
+
+        }
+	}
+}
