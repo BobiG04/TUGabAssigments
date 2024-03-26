2840714fe79631b4c78172d2cf30aab67a449a5f
Bogomil Ivanov <bogomil_3098@abv.bg>
2024-03-26 09:41:07 +0200

    Code-26.03.2024

---
+++ C#/26_03_2024/6_1zad/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _6_1zad
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+
+	class Doggo
+	{
+		private string name;
+		private int age;
+
+		public Doggo () {
+			name = "";
+			age = 0;
+		}
+
+		public Doggo (string name, int age)
+		{
+			this.name = name;
+			this.age = age;
+		}
+
+		public void Input ()
+		{
+			Console.WriteLine("Enter a doggo name: ");
+			name = Console.ReadLine();
+
+            Console.WriteLine("Enter it's age: ");
+			age = int.Parse(Console.ReadLine());
+        }
+
+		public static void Input(out string name, out int age)
+		{
+			name = Console.ReadLine();
+			age = int.Parse (Console.ReadLine());
+        }
+
+		public static void Output(in string name, in int age)
+		{
+			Console.WriteLine(String.Join(" ", name, age));
+		}
+	}
+}
