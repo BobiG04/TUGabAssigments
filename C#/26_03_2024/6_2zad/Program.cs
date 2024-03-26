4ed00dde7ed436f13ec03cfc77dcc2c8e77431da
Bogomil Ivanov <bogomil_3098@abv.bg>
2024-03-26 14:43:02 +0200

    Code-26.03.2024

---
+++ C#/26_03_2024/6_2zad/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _6_2zad
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			Person human1 = new Person();
+			Person human2 = new Person();
+			Person human3 = new Person();
+
+			human1.Input();
+			human2.Input();
+			human3.Input();
+
+			human1.Output(2024);
+			human2.Output(2024);
+			human3.Output(2024);
+
+		}
+	}
+
+	class Person
+	{
+		string firstName;
+		string middleName;
+		string lastName;
+		int birthYear;
+		int age;
+
+		public Person () {
+			firstName = "";
+			middleName = "";
+			lastName = "";
+			birthYear = 0;
+			age = 0;
+		}
+
+		public void Input()
+		{
+			firstName = Console.ReadLine();
+			middleName = Console.ReadLine();
+			lastName = Console.ReadLine();
+			birthYear = int.Parse(Console.ReadLine());
+		}
+
+		public void Output (in int currentYear)
+		{
+			age = currentYear - birthYear;
+			Console.WriteLine(String.Join(" ", firstName, middleName, lastName, age));
+		}
+	}
+}
---
+++ C#/26_03_2024/6_3zad/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _6_3zad
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			int n = int.Parse(Console.ReadLine());
+			Triangle[] tris = new Triangle[n];
+
+			double p;
+			double s;
+
+            for (int i = 0; i < tris.Length; i++)
+            {
+				tris[i] = new Triangle();
+				tris[i].Input();
+				tris[i].Calculations(tris[i],out p, out s);
+				Console.WriteLine("The perimeter and the area of triangle " + i + " is respectively " + p + " & " + s);
+            }
+        }
+	}
+
+	class Triangle
+	{
+		private double a;
+		private double b;
+		private double c;
+		private bool isTris;
+
+		public Triangle()
+		{
+			a = 0.0; b = 0.0; c = 0.0; isTris = false;
+		}
+
+		public void Input()
+		{
+			a = int.Parse(Console.ReadLine());
+			b = int.Parse(Console.ReadLine());
+			c = int.Parse(Console.ReadLine());
+
+			if (a + b > c && b + c > a && a + c > b)
+			{
+				isTris = true;
+			}
+			else
+			{
+				isTris = false;
+			}
+		}
+
+		public void Calculations(in Triangle trisObj, out double p, out double s) 
+		{
+			p = 0.0;
+			s = 0.0;
+		
+			if (trisObj.isTris)
+			{
+				p = trisObj.a + trisObj.b + trisObj.c;
+				double halfp = p / 2;
+				s = Math.Round(Math.Sqrt(halfp * (halfp - trisObj.a) * (halfp - trisObj.b) * (halfp - trisObj.c)), 2);
+			} else
+			{
+                Console.WriteLine("ERROOOOR! Triangle is non-existant.");
+				p = 0.0;
+				s = 0.0;
+            }
+
+		}
+	}
+}
