2b7fe0909407352704792edc019263277fc8905b
Bogomil Ivanov <bogomil.g.ivanov@gmail.com>
2025-06-29 18:35:27 +0300

    new project

---
+++ ППИ/cv-site/src/App.jsx
@@ -0,0 +1,22 @@
+import 'tailwindcss'
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div class="main">
+      <nav>
+        <ul>
+          <li><button onClick>За мен</button></li>
+          <li><button>Образование</button></li>
+          <li><button onClick>Контакти</button></li>
+        </ul>
+      </nav>
+      
+    </div>
+  )
+  
+}
+
+export default App
