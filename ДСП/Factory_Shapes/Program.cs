8fed7bd773108a573da91153fbc6b6d999c14e92
Bogomil Ivanov <bogomil.g.ivanov@gmail.com>
2025-10-28 08:32:57 +0200

    Code-28.10.2025

---
+++ ДСП/Factory_Shapes/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+
+namespace Factory_Shapes
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Enter the number of shapes: ");
+			int numbObjects = int.Parse(Console.ReadLine());
+			Creator[] creators = new Creator[numbObjects];
+			Shape[] shapes = new Shape[numbObjects];
+
+			string command = "";
+			for (int i = 0; i < shapes.Length; i++)
+			{
+				Console.Write("List of commands: \n 1 Make a Circle \n 2 Make a Rectangle \n 3 Make a Square \n 4 Check List \n 5 Calculate Areas \n 6 End Program \nEnter a command: ");
+				command = Console.ReadLine();
+				switch (command)
+				{
+					case "Make a Circle" or "1":
+						Console.Write("Enter the radius: ");
+						int r = int.Parse(Console.ReadLine());
+						creators[i] = new CircleCreator(r);
+						shapes[i] = creators[i].FactoryMethod();
+						Console.WriteLine("Created {0}", shapes[i].GetType().Name);
+						break;
+					case "Make a Rectangle" or "2":
+						Console.Write("Enter the width: ");
+						int w = int.Parse(Console.ReadLine());
+						Console.Write("Enter the height: ");
+						int h = int.Parse(Console.ReadLine());
+						creators[i] = new RectangleCreator(w, h);
+						shapes[i] = creators[i].FactoryMethod();
+						Console.WriteLine("Created {0}", shapes[i].GetType().Name);
+						break;
+					case "Make a Square" or "3":
+						Console.Write("Enter the side lenght: ");
+						int a = int.Parse(Console.ReadLine());
+						creators[i] = new SquareCreator(a);
+						shapes[i] = creators[i].FactoryMethod();
+						Console.WriteLine("Created {0}", shapes[i].GetType().Name);
+						break;
+					case "Check List" or "4":
+						foreach (var shape in shapes)
+						{
+							Console.WriteLine("{0}", shape);
+						}
+						i--;
+						break;
+					case "Calculate Areas" or "5":
+						Console.Write("Enter the number in the list of shapes: ");
+						int listNum = int.Parse(Console.ReadLine());
+						for (int j = 0; j < shapes.Length; j++)
+						{
+							if (j == listNum && shapes[j] != null)
+								Console.WriteLine("The area of {0} is: {1}", shapes[j].GetType().Name, shapes[j].Area());
+							else
+							{
+								Console.WriteLine("No shape found!");
+								return;
+							}
+						}
+						i--;
+						break;
+					case "End Program" or "6":
+						return;
+					default:
+						break;
+				}
+			}
+
+			do
+			{
+				Console.Write("List of commands: \n 4 Check List \n 6 End program \nEnter a command: ");
+				command = Console.ReadLine();
+				switch (command)
+				{
+					case "Check List" or "4":
+						foreach (var shape in shapes)
+						{
+							Console.WriteLine("{0}", shape);
+						}
+						break;
+					case "Calculate Areas" or "5":
+						int listNum = int.Parse(Console.ReadLine());
+						foreach (var item in shapes)
+						{
+							Console.WriteLine("The area of {0} is: {1}", item.GetType().Name, item.Area());
+						}
+						break;
+					case "End Program" or "6":
+						return;
+					default:
+						break;
+				}
+			} while (command != "5");
+
+			/*creators[0] = new CircleCreator();
+			creators[1] = new RectangleCreator();
+			creators[2] = new SquareCreator();
+
+			foreach (var creator in creators) {
+				Shape shape = creator.FactoryMethod();
+				Console.WriteLine("Created {0}", shape.GetType().Name);
+			}*/
+		}
+	}
+
+	abstract class Shape
+	{
+		public abstract double Area();
+	}// Product class
+	
+	class Circle : Shape // Product A
+	{
+		protected int radius;
+
+		public int Radius { get { return radius; } set { radius = value; }  }
+
+		public Circle(int r)
+		{
+			Radius = r;
+		}
+
+		public override double Area()
+		{
+			return Math.PI * radius * radius;
+		}
+	}
+
+	class Rectangle : Shape // Product B
+	{
+		protected int width;
+		protected int height;
+
+		public int Width { get { return width;  } set { width = value; }  }
+		public int Height { get { return height; } set { height = value; }  }
+
+		public Rectangle(int w, int h)
+		{
+			Width = w;
+			Height = h;
+		}
+
+		public override double Area()
+		{
+			return width * height;
+		}
+	}
+
+	class Square : Shape // Product C
+	{
+		protected int a;
+		public int A { get { return a; } set { a = value; }  }
+
+		public Square (int aSide)
+		{
+			A = aSide;
+		}
+
+		public override double Area()
+		{
+			return a * a;
+		}
+	}
+
+	abstract class Creator
+	{
+		public abstract Shape FactoryMethod();
+	}
+
+	class CircleCreator (int r) : Creator
+	{
+		public override Shape FactoryMethod()
+		{
+			return new Circle(r);
+		}
+	}
+
+	class RectangleCreator (int w, int h) : Creator
+	{
+		public override Shape FactoryMethod()
+		{
+			return new Rectangle(w, h);
+		}
+	}
+
+	class SquareCreator (int a) : Creator
+	{
+		public override Shape FactoryMethod()
+		{
+			return new Square(a);
+		}
+	}
+}
---
+++ ДСП/Furniture/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+
+namespace Furniture
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Creator> creators = new List<Creator>();
+			List<FurnitureProducts> products = new List<FurnitureProducts>();
+			string command;
+			int width, height;
+
+			do
+			{
+				Console.Write("List of commands: \n1 Add Chair\n2 Add Table\n3 Add Bench\n4 List items\n5 Finish order\n\nEnter a command:");
+				command = Console.ReadLine();
+				Console.Clear();
+				switch (command)
+				{
+					case "Add Chair" or "1":
+						Console.Write("Enter width: ");
+						width = int.Parse(Console.ReadLine());
+						Console.Write("Enter height: ");
+						height = int.Parse(Console.ReadLine());
+						creators.Add(new ChairCreator());
+						products.Add(creators[creators.Count - 1].FactoryMethod(width, height));
+						Console.WriteLine("Item: {0} has been added!\n", products[products.Count - 1].GetType().Name);
+						break;
+					case "Add Table" or "2":
+						Console.Write("Enter width: ");
+						width = int.Parse(Console.ReadLine());
+						Console.Write("Enter height: ");
+						height = int.Parse(Console.ReadLine());
+						creators.Add(new TableCreator());
+						products.Add(creators[creators.Count - 1].FactoryMethod(width, height));
+						Console.WriteLine("Item: {0} has been added!\n", products[products.Count - 1].GetType().Name);
+						break;
+					case "Add Bench" or "3":
+						Console.Write("Enter width: ");
+						width = int.Parse(Console.ReadLine());
+						Console.Write("Enter height: ");
+						height = int.Parse(Console.ReadLine());
+						creators.Add(new BenchCreator());
+						products.Add(creators[creators.Count - 1].FactoryMethod(width, height));
+						Console.WriteLine("Item: {0} has been added!\n", products[products.Count - 1].GetType().Name);
+						break;
+					case "List items" or "4":
+						if (products.Count <= 0)
+						{
+							Console.WriteLine("No items in the list.");
+						}
+						else
+						{
+							Console.WriteLine("The items in the order:");
+							int i = 1;
+							foreach (var item in products)
+							{
+								Console.WriteLine("{0} {1} with dimensions: {2}", i, item.GetType().Name, item.GetInfo());
+								i++;
+							}
+						}
+						Console.WriteLine(" ");
+						break;
+					case "Finish order" or "5":
+						Console.Clear();
+						break;
+					default:
+						Console.WriteLine("Unrecognised command!\n");
+						break;
+				}
+				if (command == "5" || command == "Finish order")
+				{
+					break;
+				}
+			}
+			while (true);
+
+			if (products.Count <= 0)
+			{
+				Console.WriteLine("No items ordered. Have a nice day!");
+			}
+			else
+			{
+				Console.WriteLine("Order of: ");
+				foreach (var item in products)
+				{
+					Console.WriteLine(item);
+				}
+				Console.WriteLine("is finished! Have a nice day!");
+			}
+
+		}
+	}
+}
+
+interface FurnitureProducts
+{
+	string GetInfo();
+}
+
+class Chair : FurnitureProducts
+{
+
+	protected int width, height;
+
+	public int Width { get { return width; } set { width = value; } }
+	public int Height { get { return height; } set { height = value; } }
+
+	public Chair(int w, int h)
+	{
+		Width = w;
+		Height = h;
+	}
+
+	public string GetInfo()
+	{
+		return width.ToString() + " " + height.ToString();
+	}
+
+}
+
+class Table : FurnitureProducts
+{
+	protected int width, height;
+
+	public int Width { get { return width; } set { width = value; } }
+	public int Height { get { return height; } set { height = value; } }
+
+	public Table(int w, int h)
+	{
+		Width = w;
+		Height = h;
+	}
+
+	public string GetInfo()
+	{
+		return width.ToString() + " " + height.ToString();
+	}
+}
+
+class Bench : FurnitureProducts
+{
+	protected int width, height;
+
+	public int Width { get { return width; } set { width = value; } }
+	public int Height { get { return height; } set { height = value; } }
+
+	public Bench(int w, int h)
+	{
+		Width = w;
+		Height = h;
+	}
+
+	public string GetInfo()
+	{
+		return width.ToString() + " " + height.ToString();
+	}
+}
+
+interface Creator
+{
+	public FurnitureProducts FactoryMethod(int w, int h);
+}
+
+class ChairCreator : Creator
+{
+	public FurnitureProducts FactoryMethod(int w, int h)
+	{
+		return new Chair(w, h);
+	}
+}
+
+class TableCreator : Creator
+{
+	public FurnitureProducts FactoryMethod(int w, int h)
+	{
+		return new Table(w, h);
+	}
+}
+
+class BenchCreator : Creator
+{
+	public FurnitureProducts FactoryMethod(int w, int h)
+	{
+		return new Bench(w, h);
+	}
+}
