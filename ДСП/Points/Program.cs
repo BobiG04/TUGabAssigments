982bca18f636fd94b7d647a5c73207c9fd92b15d
Bogomil Ivanov <bogomil.g.ivanov@gmail.com>
2025-11-05 10:53:45 +0200

    Code-05.11.2025

--- ДСП/Points/Program.cs
+++ ДСП/Points/Program.cs
@@ -10,7 +10,7 @@
 			Console.Write("Enter the number of points: ");
 			int n = int.Parse(Console.ReadLine());
 
-			Point[] points = new Point[n];
+			List<Point> points = new List<Point>(new Point[n]);
 
 			for (int i = 0; i < n; i++)
 			{
